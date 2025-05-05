6ec06c68e0de7ac531aca4b6e8411b80a923f8e8
Einar Gangsø <mail@einargangso.no>
2025-05-05 08:19:26 +0200

    Do not use CDN

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import webfontDownload from 'vite-plugin-webfont-dl';
 
 export default defineConfig({
     plugins: [
         tailwindcss(),
+        webfontDownload(),
     ],
 
     base: '/',
